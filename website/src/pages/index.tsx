39f869bb2b944fcd680dad03196a5455ca53e232
shelepuginivan <shelepuginivanm@gmail.com>
2023-10-28 12:27:40 +0300

    feat(website): remove default components

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -3,7 +3,6 @@ import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 import Layout from '@theme/Layout'
-import HomepageFeatures from '@site/src/components/HomepageFeatures'
 
 import styles from './index.module.css'
 
@@ -47,9 +46,7 @@ export default function Home(): JSX.Element {
             description="Description will go into a meta tag in <head />"
         >
             <HomepageHeader />
-            <main>
-                <HomepageFeatures />
-            </main>
+            <main></main>
         </Layout>
     )
 }
