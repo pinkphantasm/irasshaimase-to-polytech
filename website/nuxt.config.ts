22dcaea99c0d00d2ff892f05051877f1e2d25ed2
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-20 02:11:29 +0300

    build(website): use static site generation

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -1,4 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
+    hooks: {
+        'prerender:routes'({ routes }) {
+            routes.clear()
+        },
+    },
 })
