4540b698f0944a2238320d08b9a1e022a65e8299
shelepuginivan <shelepuginivanm@gmail.com>
2024-06-20 02:23:09 +0300

    feat(website): configure layouts

---
+++ website/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="wrapper">
+        <slot />
+    </div>
+</template>
+
+<style scoped lang="sass">
+.wrapper
+    display: flex
+    flex-direction: column
+    height: 100%
+</style>
